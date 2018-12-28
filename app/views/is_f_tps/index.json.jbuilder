c1ef9f886ad3697901f38a0fe1feb37496082a4f
Vadim <vadimmalakhovski@hotmail.com>
2018-12-28 09:14:31 +0000

    heroku from beginning working

--- app/views/is_f_tps/index.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.array! @is_f_tps, partial: 'is_f_tps/is_f_tp', as: :is_f_tp
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :is_f_tps
   resources :bulletins
   resources :widgets
 
