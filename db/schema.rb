021c274afdfaab0de34350d95cd5823fc3868e3f
Vadim <vadimmalakhovski@hotmail.com>
2018-12-31 11:49:41 +0000

    heroku bad commit restore2

--- db/schema.rb
+++ db/schema.rb
@@ -54,31 +54,7 @@ ActiveRecord::Schema.define(version: 2018_12_31_113858) do
   end
 
 
-  create_table "mappings", force: :cascade do |t|
-    t.string "a1"
-    t.string "b2"
-    t.string "c3"
-    t.string "d4"
-    t.string "e5"
-    t.string "f6"
-    t.string "g7"
-    t.string "h8"
-    t.string "i9"
-    t.string "j10"
-    t.string "k11"
-    t.string "l12"
-    t.string "m13"
-    t.string "n14"
-    t.string "x24"
-    t.string "y25"
-    t.string "z26"
-    t.string "aa27"
-    t.string "ad30"
-    t.string "ae31"
-    t.string "af32"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ 
 
   create_table "widgets", force: :cascade do |t|
     t.string "name"
