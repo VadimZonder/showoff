40c5d3e47034c37ac89d6479db12896bb139d1d2
Vadim <vadimmalakhovski@hotmail.com>
2019-01-17 16:33:51 +0000

    heroku JS 1 click print heroku test

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,7 +8,7 @@ class WelcomeController < ApplicationController
     if @url.include? 'c9users.io' 
         @deployment = false
     else
-        @deployment = true
+        @deployment = false ####change to true
         
     end  
       
