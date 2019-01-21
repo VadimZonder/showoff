c940466b167901bb96c5375629167b6643ff71a5
Vadim <vadimmalakhovski@hotmail.com>
2019-01-21 14:34:46 +0000

    heroku urls from var2

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -130,9 +130,9 @@ class WelcomeController < ApplicationController
             ###CHANGE LATER
             if @deployment == true
                 @debug3 = true
-                  @csvFileLocationOpen = open('https://label-gen-db2.herokuapp.com/uploads/resume/attachment/1/OurFormatTest.csv')
+                  @csvFileLocationOpen = open('https://label-gen-var2.herokuapp.com/uploads/resume/attachment/1/OurFormatTest.csv')
                 open('OurFormatTest.csv', 'wb') do |file|
-                   file << open('https://label-gen-db2.herokuapp.com/uploads/resume/attachment/1/OurFormatTest.csv').read
+                   file << open('https://label-gen-var2.herokuapp.com/uploads/resume/attachment/1/OurFormatTest.csv').read
                    @file5 = file
                    
                  end
