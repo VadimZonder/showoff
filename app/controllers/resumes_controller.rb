11996c7ef6c28419c465b6fc62a767b9eff81a40
Vadim <vadimmalakhovski@hotmail.com>
2019-01-28 09:23:03 +0000

    heroku csv to ftp upload

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -9,6 +9,22 @@ class ResumesController < ApplicationController
    end
    
    def create
+           require 'csv' 
+    require 'net/ftp'
+    require 'tempfile'
+    require "open-uri"
+    require 'rest-client'
+    
+    ##Development or Deployment____________
+    @url =  request.original_url
+    if @url.include? 'c9users.io' 
+        @deployment = false
+    else
+        @deployment = true
+        
+    end  
+    
+    
      #checks if there are Resume entries in DB
       if  Resume.first.nil?
         #true=make new one
@@ -19,6 +35,115 @@ class ResumesController < ApplicationController
         Resume.last.destroy
         @resume = Resume.new(resume_params)
         # @resume = current_user.resumes.new(resume_params)
+        
+
+
+#Upload to FTP
+ftp = Net::FTP::new("ftp.dpd.ie")
+ftp.login("3L4", "3l4123")
+ftp.chdir("/users/3L4/WebAppImport")
+
+
+
+  ftp.passive = true
+  #ftp.putbinaryfile('Gemfile')
+ # tempfile = Tempfile.new('test')
+  #tempfile.write("This is some text data I want to upload via FTP.")
+  #my_data = "This is some text data I want to upload via FTP."
+ # my_data = Bulletin.last
+ # tempfile.puts my_data
+ # ftp.putbinaryfile(tempfile)
+
+
+# name_start = 'my_special_file'
+#name_end = '.txt'
+#location = '/path/to/some/dir'
+options = { encoding: Encoding::UTF_8 }
+#tempfile = Tempfile.new([name_start, name_end],  options) do |file|
+# file.write('Hello, tempfile!')
+#end
+
+tempfile = Tempfile.new(['test', '.csv'], options)
+#tempfile.write("22This is some text data I want to upload via FTP.")
+
+#get the link of the file
+##tempfile.write(@bulletin2 =   rails_blob_path(Bulletin.last.attachment))
+
+#@bulletin2 = Bulletin.last.attachment.variant, rails_blob_path( Bulletin.last.attachment, disposition: "attachment")
+#@bulletin2 = File.read(rails_blob_path(Bulletin.last.attachment))
+
+
+#@open_uri = open(rails_blob_path(Bulletin.last.attachment))
+####################@csvFileLocation3 = open('https://api-to-labels-base-vadimmalakhovski.c9users.io'+rails_blob_path(Bulletin.last.attachment))
+######@csvFileLocation3 = open('https://label-gen-is-ftp.herokuapp.com'+rails_blob_path(Bulletin.last.attachment))
+####################@open('OurFormatTest.csv', 'wb') do |file|
+####################@  file << open('https://api-to-labels-base-vadimmalakhovski.c9users.io'+rails_blob_path(Bulletin.last.attachment)).read
+ ##### file << open('https://label-gen-is-ftp.herokuapp.com'+rails_blob_path(Bulletin.last.attachment)).read
+####################@  @file = file
+####################@end
+
+####################@@bulletin3 = CSV.read(@file)
+
+if deployment = false
+@csvFileLocation3 = open('https://api-to-labels-base-vadimmalakhovski.c9users.io'+@resume.attachment.to_s)
+######@csvFileLocation3 = open('https://label-gen-is-ftp.herokuapp.com'+rails_blob_path(Bulletin.last.attachment))
+open('OurFormatTest.csv', 'wb') do |file|
+file << open('https://api-to-labels-base-vadimmalakhovski.c9users.io'+@resume.attachment.to_s).read
+ ##### file << open('https://label-gen-is-ftp.herokuapp.com'+rails_blob_path(Bulletin.last.attachment)).read
+  @file = file
+end
+
+
+else
+ @csvFileLocation3 = open('https://label-gen-var3.herokuapp.com'+@resume.attachment.to_s)
+######@csvFileLocation3 = open('https://label-gen-is-ftp.herokuapp.com'+rails_blob_path(Bulletin.last.attachment))
+open('OurFormatTest.csv', 'wb') do |file|
+file << open('https://label-gen-var3.herokuapp.com'+@resume.attachment.to_s).read
+ ##### file << open('https://label-gen-is-ftp.herokuapp.com'+rails_blob_path(Bulletin.last.attachment)).read
+  @file = file
+end   
+end
+
+####################@@bulletin3 = CSV.read(@file)
+@fileReadCSV = CSV.read(@file)
+##@csvFileLocation2 = rails_blob_path(Bulletin.last.attachment, disposition: "attachment")
+##@bulletin2 = CSV.read(@csvFileLocation2.chomp("/*")) 
+
+#@csvFileLocation2 =  File.open('/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCZz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--4426c02a8060d8ac6f3ea960420e0a1417f330f0/OurFormatTest.csv')
+#@csvFileLocation2 =  'https://api-to-labels-base-vadimmalakhovski.c9users.io/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCZz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--4426c02a8060d8ac6f3ea960420e0a1417f330f0/OurFormatTest.csv?disposition=attachment'
+
+
+####################@tempfile.write(@bulletin3)
+####################tempfile.write(@resume.attachment)
+tempfile.write(@fileReadCSV)
+
+#link_to "Download my", rails_blob_path(Bulletin.last.attachment, disposition: "attachment")
+
+#@bulletin2 = File.read(rails_blob_path(@bulletin_list.last.attachment, disposition: "attachment"))
+#tempfile.write(@bulletin2 = Bulletin.last.attachment)
+#tempfile.write(@bulletin2 = Bulletin.order(created_at: :desc).all)
+#tempfile.write(@bulletin2 = File.read(rails_blob_path(bulletin.attachment)))
+
+#= link_to(image_tag(bulletin.attachment.variant(resize: "200x200")), rails_blob_path(bulletin.attachment, disposition: "attachment"))
+#= link_to "Download file", rails_blob_path(bulletin.attachment, disposition: "attachment")
+
+
+tempfile.rewind
+ftp.putbinaryfile(tempfile)
+  
+####################@  @bulletin3  = tempfile.read
+  
+  tempfile.unlink
+
+
+
+
+
+ftp.close
+tempfile.close
+
+
+        
         
       end
       
